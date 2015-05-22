0765da372ad2a256e177c54145bed7f43d388bb2
Ds1gn <i.d.mackinnon@gmail.com>
2015-05-22 01:03:15 -0400

    ability to mark as complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
   resources :todo_lists do
-    resources :todo_items
-end
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
+  end
   root "todo_lists#index"
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,4 +63,3 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
